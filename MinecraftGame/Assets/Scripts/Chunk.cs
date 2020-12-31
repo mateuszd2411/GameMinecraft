1114164d97c6ba4b4eb83e309b447c3f08b08414
mateuszd2411 <mateuszd2411@gmail.com>
2020-12-31 14:43:49 +0100

    Refactored Chunk.cs

--- MinecraftGame/Assets/Scripts/Chunk.cs
+++ MinecraftGame/Assets/Scripts/Chunk.cs
@@ -7,14 +7,20 @@ public class Chunk : MonoBehaviour
     public MeshRenderer meshRenderer;
     public MeshFilter meshFilter;
 
+    int vertexIndex = 0;
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    List<Vector2> uvs = new List<Vector2>();
+
     void Start()
     {
-        int vertexIndex = 0;
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
-        List<Vector2> uvs = new List<Vector2>();
+        AddVoxelDataToChunk();
+        CreateMash();
+    }
 
-        for(int p = 0; p < 6; p++)
+    void AddVoxelDataToChunk()
+    {
+        for (int p = 0; p < 6; p++)
         {
             for (int i = 0; i < 6; i++)
             {
@@ -22,13 +28,15 @@ public class Chunk : MonoBehaviour
                 vertices.Add(VoxelData.voxelVerts[triangelIndex]);
                 triangles.Add(vertexIndex);
 
-                uvs.Add(VoxelData.voxelUvs [i]);
+                uvs.Add(VoxelData.voxelUvs[i]);
 
                 vertexIndex++;
             }
         }
-        
+    }
 
+    void CreateMash()
+    {
         Mesh mesh = new Mesh();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
