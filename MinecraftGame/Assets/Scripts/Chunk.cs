4a22d1765ea4dda653d476fc940b61bf78fc0a24
mateuszd2411 <mateuszd2411@gmail.com>
2020-12-31 11:46:05 +0100

    Added Chunk.cs
    Added for loop (read parameters for  square  )

--- MinecraftGame/Assets/Scripts/Chunk.cs
+++ MinecraftGame/Assets/Scripts/Chunk.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Chunk : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public MeshRenderer meshRenderer;
+    public MeshFilter meshFilter;
+
     void Start()
     {
-        
+        int vertexIndex = 0;
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            int triangelIndex = VoxelData.voxelTris[0, i];
+            vertices.Add(VoxelData.voxelVerts[triangelIndex]);
+            triangles.Add(vertexIndex);
+
+            vertexIndex++;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
