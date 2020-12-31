683a4b27edea9c27db4abd8e6e9a92c5f0b1a0ca
mateuszd2411 <mateuszd2411@gmail.com>
2020-12-31 12:39:13 +0100

    Set material for Chunk gameobject
    Added line of code responsible for adding material for chunks

--- MinecraftGame/Assets/Scripts/Chunk.cs
+++ MinecraftGame/Assets/Scripts/Chunk.cs
@@ -12,6 +12,7 @@ public class Chunk : MonoBehaviour
         int vertexIndex = 0;
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
+        List<Vector2> uvs = new List<Vector2>();
 
         for (int i = 0; i < 6; i++)
         {
@@ -19,12 +20,15 @@ public class Chunk : MonoBehaviour
             vertices.Add(VoxelData.voxelVerts[triangelIndex]);
             triangles.Add(vertexIndex);
 
+            uvs.Add(Vector2.zero);
+
             vertexIndex++;
         }
 
         Mesh mesh = new Mesh();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
+        mesh.uv = uvs.ToArray();
 
         mesh.RecalculateNormals();
 
