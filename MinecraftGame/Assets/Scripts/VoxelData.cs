09e495b090c5d058d1feb1e8b0f03c05808c77a5
mateuszd2411 <mateuszd2411@gmail.com>
2020-12-31 12:48:55 +0100

    Added parameters for square for all side

--- MinecraftGame/Assets/Scripts/VoxelData.cs
+++ MinecraftGame/Assets/Scripts/VoxelData.cs
@@ -18,6 +18,11 @@ public static class VoxelData
 
     public static readonly int[,] voxelTris = new int[1, 6]
     {
+        {0,3,1,1,3,2} //Back Face
+        {5,6,4,4,6,7} //Front Face
         {3,7,2,2,7,6} //Top Face
+        {1,5,0,0,5,4} //Bottom Face
+        {4,7,0,0,7,3} //Left Face
+        {1,2,5,5,2,6} //Right Face
     };
 }
